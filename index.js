25309d366686d1cdafa29f2b222870cee0f317c2
Cheng Vue <cvu3@ucsc.edu>
2018-11-05 18:25:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHoliday() {
+  return "Happy Holidays!";
+}
+
+function happyHolidayTo(name) {
+  return `Happy Holiday, ${name}`;
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}`;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}`;
+}
